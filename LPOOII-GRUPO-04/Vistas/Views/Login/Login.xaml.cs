42c309630d5c75ae6530c77062c9417c8cc1242b
gabriel9898 <38196366+gabriel9898@users.noreply.github.com>
2020-08-31 15:21:53 -0300

    + Creacion de Vistas

---
+++ LPOOII-GRUPO-04/Vistas/Views/Login/Login.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using Vistas.Views.Menu;
+
+namespace Vistas.Views.Login
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow main = new MainWindow();
+            main.Show();
+        }
+    }
+}
